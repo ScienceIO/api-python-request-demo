db28261f448365fe6d2108a3f0ab119c88794de8
averypelle <avery.pelle@gmail.com>
2023-01-10 11:31:40 -0500

    run pep8 formatter

--- redact_demo.py
+++ redact_demo.py
@@ -4,14 +4,14 @@ from scio_api_tools import call_api
 
 
 @click.command()
-@click.option('--text', help='Submit text to redact-phi model')
+@click.option("--text", help="Submit text to redact-phi model")
 def redact_phi(text: str) -> dict:
     """Redact PHI in text"""
-    inference_result = call_api(model='redact-phi', text=text)
+    inference_result = call_api(model="redact-phi", text=text)
     print(f"ORIGINAL:\n{text}\n")
     print(f"REDACTED:\n{inference_result['redacted_text']}")
     return inference_result
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     inference_result = redact_phi()
